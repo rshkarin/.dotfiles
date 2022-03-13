1f817437afdb43d3fed31664283636942fac02d8
Roman Shkarin <roman.v.shkarin@gmail.com>
2022-03-13 15:57:35 +0100

    chore(plugin): change lsp highlight

--- nvim/.config/nvim/lua/config/lsp/utils.lua
+++ nvim/.config/nvim/lua/config/lsp/utils.lua
@@ -10,42 +10,21 @@ function M.lsp_diagnostics()
 
     local on_references = vim.lsp.handlers["textDocument/references"]
     vim.lsp.handlers["textDocument/references"] = vim.lsp.with(on_references, { loclist = true, virtual_text = true })
-
-    -- Send diagnostics to quickfix list
-    do
-        local method = "textDocument/publishDiagnostics"
-        local default_handler = vim.lsp.handlers[method]
-        vim.lsp.handlers[method] = function(err, meth, result, client_id, bufnr, config)
-            default_handler(err, meth, result, client_id, bufnr, config)
-            local diagnostics = vim.lsp.diagnostic.get_all()
-            local qflist = {}
-            for buf, diagnostic in pairs(diagnostics) do
-                for _, d in ipairs(diagnostic) do
-                    d.bufnr = buf
-                    d.lnum = d.range.start.line + 1
-                    d.col = d.range.start.character + 1
-                    d.text = d.message
-                    table.insert(qflist, d)
-                end
-            end
-            vim.lsp.util.set_qflist(qflist)
-        end
-    end
 end
 
 function M.lsp_highlight(client, bufnr)
     if client.resolved_capabilities.document_highlight then
         vim.api.nvim_exec(
             [[
-        hi LspReferenceRead cterm=bold ctermbg=red guibg=#282f45
-        hi LspReferenceText cterm=bold ctermbg=red guibg=#282f45
-        hi LspReferenceWrite cterm=bold ctermbg=red guibg=#282f45
-        augroup lsp_document_highlight
-        autocmd! * <buffer>
-        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-        augroup END
-        ]],
+                hi LspReferenceRead cterm=bold ctermbg=DarkRed guibg=#282f45
+                hi LspReferenceText cterm=bold ctermbg=DarkRed guibg=#282f45
+                hi LspReferenceWrite cterm=bold ctermbg=DarkRed guibg=#282f45
+                augroup lsp_document_highlight
+                    autocmd! * <buffer>
+                    autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+                    autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+                augroup END
+            ]],
             false
         )
     end

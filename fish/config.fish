4a16070e5c97867e3f6c7b0b887504130f38cb31
Roman Shkarin <roman.v.shkarin@gmail.com>
2024-02-03 23:08:41 +0100

    Update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,8 @@
 set -gx GOPRIVATE github.com/remerge
 starship init fish | source
 source /usr/local/opt/asdf/libexec/asdf.fish
+
+# Created by `pipx` on 2023-06-22 08:52:01
+set PATH $PATH /Users/roman/.local/bin
+set PATH $PATH /Users/roman/.cargo/bin
+set PATH $PATH /Users/roman/.config/scripts

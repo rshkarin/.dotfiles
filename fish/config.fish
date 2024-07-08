7747afbd4cb33c044d4b2b59b485ba13870da138
Roman Shkarin <roman.v.shkarin@gmail.com>
2024-07-08 12:56:08 +0200

    Change bin paths to brew

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,17 @@
 set -gx GOPRIVATE github.com/remerge
-set -gx EDITOR /usr/local/bin/nvim
+set -gx EDITOR $(brew --prefix)/bin/nvim
 
 starship init fish | source
 
-source /usr/local/opt/asdf/libexec/asdf.fish
+set PATH $PATH /usr/sbin
+
+source $(brew --prefix)/opt/asdf/libexec/asdf.fish
 source $HOME/.config/fish/aliases.fish
 
-# Created by `pipx` on 2023-06-22 08:52:01
-set PATH $PATH /Users/roman/.local/bin
-set PATH $PATH /Users/roman/.cargo/bin
-set PATH $PATH /Users/roman/.config/scripts
-set PKG_CONFIG_PATH $PKG_CONFIG_PATH $(brew --prefix)/lib/pkgconfig
+set PATH $PATH $HOME/.local/bin
+set PATH $PATH $HOME/.cargo/bin
+set PATH $PATH $HOME/.config/scripts
+
+### MANAGED BY RANCHER DESKTOP START (DO NOT EDIT)
+set --export --prepend PATH "/Users/roman/.rd/bin"
+### MANAGED BY RANCHER DESKTOP END (DO NOT EDIT)
